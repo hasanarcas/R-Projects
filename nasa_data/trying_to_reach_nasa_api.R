ffacf9cb7850886bccfcec8fd84bfa0d249def0b
hasanarcas <hasanarcas@gmail.com>
2021-11-20 16:23:07 +0300

    Changed nasa_data

--- nasa_data/trying_to_reach_nasa_api.R
+++ nasa_data/trying_to_reach_nasa_api.R
@@ -2,5 +2,5 @@ library("httr")
 library("jsonlite")
 api_key <- "QNiqK7NtFFA9SCe4O84PiAVTPg5SKTLxbs538Dzn"
 
-HEAD <- "https://api.nasa.gov/apod/"
+HEAD <- get(paste("https://api.nasa.gov/planetary/apod/", api_key, sep=""))
 
