2f371e5be893f7eba7e579688b0fa6865b880ba6
hasanarcas <hasanarcas@gmail.com>
2021-12-08 23:46:13 +0300

    Updated the scraping script

--- Animals data/scraping.R
+++ Animals data/scraping.R
@@ -1,5 +1,6 @@
 library(rvest)
 library(dplyr)
+library(RCurl)    ## check for URL exists
 
 link <- "https://a-z-animals.com/animals/"
 page <- read_html(link)
@@ -8,10 +9,28 @@ animals_list <- page %>%
   html_nodes(".col-sm-6") %>% 
   html_text()
 
-animals_list <- gsub(" ", "-", animals_list)
-
-
-
-for(i in animals_list){
-  new_link <- paste(link,i,sep="")
+get_animal_link <- function(animals_list){
+  animal_page <- paste(link, animals_list, sep="")
+  return(animal_page)
 }
+
+links <- sapply(animals_list,get_animal_link,USE.NAMES = F)
+
+animals <- data.frame(matrix(ncol = 7, nrow = 0))
+col_names <- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Scientific Name")
+colnames(animals) <- col_names
+
+get_animal_features <- function(link){
+  if (url.exists(link)){
+    new_page <- read_html(link)
+    features <- new_page %>% 
+      html_nodes(".col-sm-9") %>% 
+      html_text()
+    if (length(features) == 7 ) {
+      animals[nrow(animals) + 1,] = features
+    }
+  }
+  
+}
+
+sapply(links, get_animal_features)
--- scraping_trials/imdb_scrape.R
+++ scraping_trials/imdb_scrape.R
@@ -5,8 +5,21 @@ link <- "https://www.imdb.com/search/title/?genres=adventure&sort=user_rating,de
 page <- read_html(link)
 
 name <- page %>% html_nodes(".lister-item-header a") %>% html_text()
+movie_links <- page %>% html_nodes(".lister-item-header a") %>% 
+  html_attr("href") %>% paste("https://www.imdb.com", ., sep="")
 year <- page %>% html_nodes(".text-muted.unbold") %>% html_text()
 rating <- page %>% html_nodes(".ratings-imdb-rating strong") %>% html_text()
 synopsis <- page %>% html_nodes(".ratings-bar+ .text-muted") %>% html_text()
 
-movies <- data.frame(name, year, rating, synopsis, stringsAsFactors = F)
+get_cast <-  function(movie_links){
+  movie_page <- read_html(movie_links)
+  movie_cast <- movie_page %>% html_nodes(".primary_photo+ td a") %>% 
+    html_text()
+  return(movie_cast)
+}
+
+cast <- sapply(movie_links, get_cast, USE.NAMES = F)
+
+
+movies <- data.frame(name, year, rating, synopsis, cast, stringsAsFactors = F)
+
